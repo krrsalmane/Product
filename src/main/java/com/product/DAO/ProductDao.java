b5f5244268d5e07da1f47f1b1407d1a6a7a01f8b
krrsalmane <karrounsalmane@gmail.com>
2025-02-14 15:05:35 +0100

    first commit

---
+++ src/main/java/com/product/DAO/ProductDao.java
@@ -0,0 +1,5 @@
+package com.product.DAO;
+
+public class ProductDao {
+
+}
---
+++ src/main/webapp/WEB-INF/Product.java
@@ -0,0 +1,10 @@
+package com.StckManagement.model;
+
+public class Product {
+private int id;
+private String name;
+private String description;
+private int productQuantity;
+private int unitPrice;
+private String categorie;
+}
