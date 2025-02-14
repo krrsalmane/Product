c265edc35daa0c3085d0c86a3056db47b41622e1
krrsalmane <karrounsalmane@gmail.com>
2025-02-14 15:55:58 +0100

    create getters and setters

--- src/main/java/com/product/DAO/ProductDao.java
+++ src/main/java/com/product/DAO/ProductDao.java
@@ -2,4 +2,33 @@ package com.product.DAO;
 
 public class ProductDao {
 
-}
+
+//creation des variables pour la connexion
+private String jdbcUrl = "jdbc:mysql://localhost:3306/stockmanager?useSSL=false";
+private String jdbcUserName= "root";
+private String jdbcPassWord ="salmane@1013";
+
+
+ private static final String INSERT_PRODUITS_SQL = "INSERT INTO produits" + "  (name, description, unitPrice, productQuantity, categorie) VALUES " +
+            " (?, ?, ?, ?, ?);";
+
+        private static final String SELECT_PRODUITS_BY_ID = "select id,name,description,unitPrice,productQuantity,categorie from product where id =?";
+        private static final String SELECT_ALL_PRODUITS = "select * from product";
+        private static final String DELETE_PRODUITS_SQL = "delete from product where id = ?;";
+        private static final String UPDATE_PRODUITS_SQL = "update product set name = ?,description= ?, unitPrice = ?,productQuantity = ?,categorie = ? where id = ?;";
+
+
+        protected Connection getConnection() {
+            Connection connection = null;
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(jdbcUrl, jdbcUserName, jdbcPassWord);
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return connection;
+        }
